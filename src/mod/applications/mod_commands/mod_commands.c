8d6bf5fc9bb845d62a57b27093f911467c537327
root <root@fs002-dev.2600hz.com>
2013-07-17 22:10:34 -0400

    emit a CHANNEL_DATA event after set so we can more efficiently keep in sync

--- src/mod/applications/mod_commands/mod_commands.c
+++ src/mod/applications/mod_commands/mod_commands.c
@@ -4985,6 +4985,8 @@ SWITCH_STANDARD_API(uuid_setvar_function)
 
 			if ((psession = switch_core_session_locate(uuid))) {
 				switch_channel_t *channel;
+                                switch_event_t *event;
+
 				channel = switch_core_session_get_channel(psession);
 
 				if (zstr(var_name)) {
@@ -4993,6 +4995,11 @@ SWITCH_STANDARD_API(uuid_setvar_function)
 				} else {
 					switch_channel_set_variable(channel, var_name, var_value);
 					stream->write_function(stream, "+OK\n");
+				}
+
+                                if (switch_event_create(&event, SWITCH_EVENT_CHANNEL_DATA) == SWITCH_STATUS_SUCCESS) {
+                                        switch_channel_event_set_data(channel, event);
+                                        switch_event_fire(&event);
 				}
 
 				switch_core_session_rwunlock(psession);
@@ -5029,6 +5036,7 @@ SWITCH_STANDARD_API(uuid_setvar_multi_function)
 
 		if ((psession = switch_core_session_locate(uuid))) {
 			switch_channel_t *channel = switch_core_session_get_channel(psession);
+			switch_event_t *event;
 			int x, y = 0;
 			argc = switch_separate_string(vars, ';', argv, (sizeof(argv) / sizeof(argv[0])));
 
@@ -5044,6 +5052,11 @@ SWITCH_STANDARD_API(uuid_setvar_multi_function)
 					switch_channel_set_variable(channel, var_name, var_value);
 					y++;
 				}
+			}
+
+			if (switch_event_create(&event, SWITCH_EVENT_CHANNEL_DATA) == SWITCH_STATUS_SUCCESS) {
+				switch_channel_event_set_data(channel, event);
+				switch_event_fire(&event);
 			}
 
 			switch_core_session_rwunlock(psession);
