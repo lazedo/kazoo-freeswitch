14121a1fc15340b5ebdbd066968b6fa441b6b254
James Aimonetti <james.aimonetti@gmail.com>
2014-09-04 14:42:29 -0700

    KAZOO-2767: default format to json

--- src/mod/event_handlers/mod_trace/mod_trace.c
+++ src/mod/event_handlers/mod_trace/mod_trace.c
@@ -36,23 +36,51 @@ SWITCH_MODULE_RUNTIME_FUNCTION(mod_trace_runtime);
 SWITCH_MODULE_LOAD_FUNCTION(mod_trace_load);
 
 typedef enum {
-EVENT_FORMAT_PLAIN,
+  EVENT_FORMAT_PLAIN,
   EVENT_FORMAT_XML,
   EVENT_FORMAT_JSON
-  } event_format_t;
+} event_format_t;
 
 static struct {
-switch_event_node_t *node;
+  switch_event_node_t *node;
+  event_format_t format;
 } globals;
 
 static void trace_handler(switch_event_t *event)
 {
-//switch_event_t *clone = NULL;
-//time_t now = switch_epoch_time_now(NULL);
+  char* trace_event;
+  char *buf;
 
-switch_assert(event != NULL);
+  switch_assert(event != NULL);
 
+  if ( !(trace_event = switch_event_get_header(event, "variable_trace_event")) ) {
+    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "ignore event %s\n", switch_event_name(event->event_id));
+    return;
+  }
 
+  if ( switch_false(trace_event) ) {
+    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "trace event was %s\n", trace_event);
+    return;
+  }
+
+  switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "tracing event %s\n", switch_event_name(event->event_id));
+
+  switch(globals.format) {
+  case EVENT_FORMAT_PLAIN:
+    break;
+  case EVENT_FORMAT_XML:
+    break;
+  case EVENT_FORMAT_JSON:
+  default:
+    if (switch_event_serialize_json(event, &buf) == SWITCH_STATUS_SUCCESS) {
+      switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "serialized event %s to json: %s\n", switch_event_name(event->event_id), buf);
+    } else {
+      switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "failed to serialize event %s\n", switch_event_name(event->event_id));
+    }
+    break;
+  }
+
+  return;
 }
 
 /* SWITCH_MODULE_DEFINITION(name, load, shutdown, runtime)
@@ -63,11 +91,16 @@ SWITCH_MODULE_DEFINITION(mod_trace, mod_trace_load, mod_trace_shutdown, NULL);
 /* Macro expands to: switch_status_t mod_trace_load(switch_loadable_module_interface_t **module_interface, switch_memory_pool_t *pool) */
 SWITCH_MODULE_LOAD_FUNCTION(mod_trace_load)
 {
-memset(&globals, 0, sizeof(globals));
+  /* connect my internal structure to the blank pointer passed to me */
+  *module_interface = switch_loadable_module_create_module_interface(pool, modname);
 
-if (switch_event_bind_removable(modname, SWITCH_EVENT_ALL, SWITCH_EVENT_SUBCLASS_ANY, trace_handler, NULL, &globals.node) != SWITCH_STATUS_SUCCESS) {
+  memset(&globals, 0, sizeof(globals));
+
+  globals.format = EVENT_FORMAT_JSON;
+
+  if (switch_event_bind_removable(modname, SWITCH_EVENT_ALL, SWITCH_EVENT_SUBCLASS_ANY, trace_handler, NULL, &globals.node) != SWITCH_STATUS_SUCCESS) {
     switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Couldn't bind!\n");
-return SWITCH_STATUS_GENERR;
+    return SWITCH_STATUS_GENERR;
   }
 
   /* indicate that the module should continue to be loaded */
@@ -76,9 +109,10 @@ return SWITCH_STATUS_GENERR;
 
 /*
   Called when the system shuts down
-  Macro expands to: switch_status_t mod_trace_shutdown() */
+  Macro expands to: switch_status_t mod_trace_shutdown()
+*/
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_trace_shutdown)
 {
-switch_event_unbind(&globals.node);
-return SWITCH_STATUS_SUCCESS;
+  switch_event_unbind(&globals.node);
+  return SWITCH_STATUS_SUCCESS;
 }
